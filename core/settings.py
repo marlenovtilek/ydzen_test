b7b688d5910891dcff25e8e0a1777084e189dbee
Tilek <marlenovtilek@gmail.com>
2024-08-09 09:36:32 +0600

    Tilek2

--- core/settings.py
+++ core/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mg$bcg_#6hw@p32a+)&!p_bxg5p=4)94z27g*d-pgb501io!)e'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-mg$bcg_#6hw@p32a+)&!p_bxg5p=4)94z27g*d-pgb501io!)e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['your-app-name.herokuapp.com','127.0.0.1']
 
 # Application definition
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
